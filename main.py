942289edcbe1dbd92faed9df8f7b1ea503b2c12b
Carter Dennis <carter.j.dennis@gmail.com>
2023-08-17 23:22:45 -0500

    Adjusting date string formatting

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def draw_scheduled_game(game: GameScoreboard):
     # TODO: Draw team records
 
     # TODO: Draw game date and start time
-    draw.text((1, 10), game.start_date, font=font, fill=white_fill)
+    draw.text((1, 18), game.start_date, font=font, fill=white_fill)
 
     # TODO: Draw betting info
 
